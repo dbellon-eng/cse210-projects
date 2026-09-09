5f4c243c77dc68cc72d02347513bf6dcf2bf149b
dbellon-eng <dbellonasencio@gmail.com>
2026-09-08 22:49:28 -0500

    Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string play ="yes";
+        while (play.ToLower()== "yes")
+        {
+            //Console.Write("What is the magic number? ");
+            //int magic= int.Parse(Console.ReadLine());
+            Random randomGenerator =new Random();
+            int magic=randomGenerator.Next(1,101);
+
+            int guess=-10;
+            while(guess!= magic)
+            {
+                Console.Write("What is your guess? ");
+                guess= int.Parse(Console.ReadLine());
+            
+                if (magic>guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magic<guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+            Console.WriteLine("Do you want to play again (yes / no)? ");
+            play= Console.ReadLine();
+            Console.WriteLine();
+
+        }
+        
     }
 }
